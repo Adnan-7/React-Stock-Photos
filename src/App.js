4f7130e60bd8b7955a8f7712b31fffe01fb9a98a
Adnan Khan <adnan7.zain@gmail.com>
2022-06-16 10:26:30 +0500

    Stock Photos Initial Fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const mainUrl = 'https://api.unsplash.com/photos/';
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url = `${mainUrl}?client_id=UNnmOmMMFOggg3zg_xLVMr4WxnM1nbLSpmQVDoTXVbg`;
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <div className='App'>
       <h2>Stock Photos Starter...</h2>
